c833522de54667095d1bcd70d5303f0f23b58eaf
riddik0331 <rrv_1981@ukr.net>
2025-03-28 03:25:07 +0200

    Добавлен модуль string Добавлена функция multiply, удален текст

--- first/nums.py
+++ first/nums.py
@@ -2,11 +2,15 @@ def plus(a, b):
     return a + b
 
 
-text = 'Hello, World!'
-
 def minus(a, b):
     return a - b
+
+
+def multiply(a, b):
+    return a * b
+
 
 if __name__ == '__main__':
     print(plus(1, 2))
     print(minus(1, 2))
+    print(multiply(3, 3))
